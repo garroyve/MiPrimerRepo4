ae6ee539634b44b6b2e4f94b7b31af8e25343496
GuilleDevo <arrovelez123@gmail.com>
2026-06-24 20:14:12 -0500

    Nuevo proyecto de API creado

---
+++ API/app.py
@@ -0,0 +1,17 @@
+import os
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/familia")
+
+def get_familia():
+    rows = ["Amin", "Marce","Miranda"]
+    return rows
+
+@app.get("/superheroesDC")
+def get_superheroesDC():
+    rows = ["Superman","Batman","Flash","Linterna Verde","Mujer Maravilla","Aquaman","Shazam","Cyborg"]
+    return rows
